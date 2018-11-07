2fd1222f20a3a687250a55cba46cfa7eeb759c90
Ruben Oostinga <ruben.oostinga@gmail.com>
2018-11-07 10:27:43 +0100

    Add deployment

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-rm -r dvbe18.confinabox.com
 ./scripts/download.sh
+rm -r dvbe18.confinabox.com
+cp -r src/dvbe18.confinabox.com .
+find dvbe18.confinabox.com -name '*#*' | xargs rm
+find dvbe18.confinabox.com -name '*\?*' | while read f; do mv "$f" "${f/\?*/}"; done
+grep -rl '<html' dvbe18.confinabox.com | grep -v '\.\(html\|js\)$' | while read filename; do mv $filename{,.html}; done
 cp scripts/offline.html dvbe18.confinabox.com
 unzip src/favicon_package_v0.16.zip -d dvbe18.confinabox.com/icons
 npm run build
